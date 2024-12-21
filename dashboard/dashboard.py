6e4897dc80e468c007ed99083e2cb3494cf26235
dhimas-jati <141932792+dhimas-jati@users.noreply.github.com>
2024-12-21 23:04:47 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -37,8 +37,8 @@ def create_count_hours_df(hours_df):
     return oneday_df
 
 
-days_df = pd.read_csv("days.csv")
-hours_df = pd.read_csv("hours.csv")
+days_df = pd.read_csv("dashboard/days.csv")
+hours_df = pd.read_csv("dashboard/hours.csv")
 
 datetime_columns = ["dteday"]
 days_df.sort_values(by="dteday", inplace=True)
@@ -59,7 +59,7 @@ max_date_hour = hours_df["dteday"].max()
 
 with st.sidebar:
     # Menambahkan logo perusahaan
-    st.image("photo.jpg")
+    st.image("dashboard/photo.jpg")
     
         # Mengambil start_date & end_date dari date_input
     start_date, end_date = st.date_input(
